0240ae1319fb365cd46567d035ff7faa41b258e5
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-06-02 17:01:27 -0700

    fix error with regex

--- basic-scrape.js
+++ basic-scrape.js
@@ -1,28 +1,32 @@
 const fs = require("fs"); // require file system from node
 const { getComponents, getName, getDesc, trim, getInputs } = require("./regex"); // require functions from regex.js
 
+// define sourceFile, reads the basic-functions.html and turns it into a string
 const sourceFileBasic = String(
    fs.readFileSync("./html-pages/basic-functions.html")
-); // define sourceFile, reads the basic-functions.html and turns it into a string
+);
+// reads intermediate-functions.html, turns into string
 const sourceFileIntermediate = String(
    fs.readFileSync("./html-pages/intermediate-functions.html")
 );
+// reads functional-library.html, turns into string
 const sourceFileFunctional = String(
    fs.readFileSync("./html-pages/functional-library.html")
 );
+// reads algorithm-functions.html, turns into string
 const sourceFileAlgorithm = String(
    fs.readFileSync("./html-pages/algorithm-functions.html")
 );
 
-const componentsBasic = getComponents(sourceFileBasic); // gets components in between each start and end comment. see regex.js
+const componentsBasic = getComponents(sourceFileBasic); // gets components in between each start and end comment from html file. see regex.js for getComponents
 const componentsIntermediate = getComponents(sourceFileIntermediate);
 const componentsFunctional = getComponents(sourceFileFunctional);
 const componentsAlgorithm = getComponents(sourceFileAlgorithm);
 
 const componentObjsBasic = componentsBasic
-   .reverse()
+   .reverse() // reverse the array of objects to put the first created at the front
    .map((component, orderIndex) => {
-      // map through components, takes a function that targets each individual key (component)
+      // map through array of objects, takes an argument that targets each individual key (component), and tracks the index of each object
       return {
          name: getName(component)[0], // run getName function which returns an array of things, then grabbing the first thing from array
          desc: trim(getDesc(component)[0]), // trim out white space, see regex.js
@@ -30,20 +34,19 @@ const componentObjsBasic = componentsBasic
          type: "basic", // scraping only the basic.html file
          typeNum: 100, // designated for basic type
          isFavorite: false, // default is false
-         order: 100 + orderIndex,
+         order: 100 + orderIndex, // take current index of object and add it to order
       };
    });
 const componentObjsIntermediate = componentsIntermediate
    .reverse()
    .map((component, orderIndex) => {
-      // map through components, takes a function that targets each individual key (component)
       return {
-         name: getName(component)[0], // run getName function which returns an array of things, then grabbing the first thing from array
-         desc: trim(getDesc(component)[0]), // trim out white space, see regex.js
-         inputs: getInputs(component).length, // counts number of inputs
-         type: "intermediate", // scraping only the basic.html file
-         typeNum: 200, // designated for basic type
-         isFavorite: false, // default is false
+         name: getName(component)[0],
+         desc: trim(getDesc(component)[0]),
+         inputs: getInputs(component).length,
+         type: "intermediate",
+         typeNum: 200,
+         isFavorite: false,
          order: 200 + orderIndex,
       };
    });
@@ -51,30 +54,30 @@ const componentObjsFunctional = componentsFunctional
    .reverse()
    .map((component, orderIndex) => {
       return {
-         name: getName(component)[0], // getName takes the parameter of component and creates an array of data, the 0 index allows us to only return the name and nothing else
+         name: getName(component)[0],
          desc: trim(getDesc(component)[0]),
-         inputs: getInputs(component).length, // returns length or number of inputs for a function
-         type: "functional", // all scraping basic.html
-         typeNum: 300, // basic type
-         isFavorite: false, // default
+         inputs: getInputs(component).length,
+         type: "functional",
+         typeNum: 300,
+         isFavorite: false,
          order: 300 + orderIndex,
       };
    });
 const componentObjsAlgorithm = componentsAlgorithm
    .reverse()
    .map((component, orderIndex) => {
-      // map through components, takes a function that targets each individual key (component)
       return {
-         name: getName(component)[0], // run getName function which returns an array of things, then grabbing the first thing from array
-         desc: trim(getDesc(component)[0]), // trim out white space, see regex.js
-         inputs: getInputs(component).length, // counts number of inputs
-         type: "algorithm", // scraping only the basic.html file
-         typeNum: 400, // designated for basic type
-         isFavorite: false, // default is false
+         name: getName(component)[0],
+         desc: trim(getDesc(component)[0]),
+         inputs: getInputs(component).length,
+         type: "algorithm",
+         typeNum: 400,
+         isFavorite: false,
          order: 400 + orderIndex,
       };
    });
 
+// created new variable which concatenates each object into a single array
 let orderedObjs = componentObjsBasic.concat(
    componentObjsIntermediate,
    componentObjsFunctional,
@@ -82,6 +85,6 @@ let orderedObjs = componentObjsBasic.concat(
 );
 
 const targetFile = "./dist/dist.json";
-console.log(orderedObjs);
 
+// write orderedObjs to dist.json as a string
 fs.writeFileSync(targetFile, JSON.stringify(orderedObjs));
